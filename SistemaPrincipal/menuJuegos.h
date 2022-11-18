eeb4a6c828562d5959937786ce23144f872ecc22
Alexisxde <olivarezalexis749@gmail.com>
2022-11-18 17:29:27 -0300

    Menu Juegos Arreglado

--- SistemaPrincipal/menuJuegos.h
+++ SistemaPrincipal/menuJuegos.h
@@ -13,14 +13,13 @@ void menuJuegos(tDatosUsuario*Userlogeado) {
     int respuesta;
     do {
     printf("\t\t---------------------------------------------------------------------\n");
-    printf("\t\t\t\t\t-*- Ultima Conexion %s -*-\n", Userlogeado->conexion);
+    printf("\t\t\t\t-*- Ultima Conexion %s -*-\n", Userlogeado->conexion);
     printf("\t\t\t\t\t\t-*- Juegos -*-\n");
     printf("\t\t\t\t\t<--- %s Tienes %d fichas --->\n", Userlogeado->nombreUsuario,Userlogeado->saldo);
     printf("\t\t---------------------------------------------------------------------\n");
     imprimirTitulos();
     if (!strcmp(Userlogeado->nombreUsuario, "admin")) {
         printf("\t\t\t\t6. Mostrar cuentas registradas de usuarios\n");
-        //* Aca van las cosas que puede hacer el admin...
     }
     printf("\t\t\t\t>>> ");
     scanf("%d", &respuesta);
@@ -36,16 +35,18 @@ void menuJuegos(tDatosUsuario*Userlogeado) {
 
 void menuSwitchJuegos(int pEleccion, tDatosUsuario *Userlogeado) {
     switch (pEleccion) {
-        
     case 1: mainmath(Userlogeado);break;
     case 2: menublackjack(Userlogeado); break;
     case 3: menuahorcado(Userlogeado);break;
     case 4: ganarSaldo(Userlogeado);break;
     case 5: printf("\t\t\t\tSaliendo...\n"); system("pause"); system("cls"); flag=false; break;
-    case 6: procesarCuentasUsuarios(); system("pause"); system("cls"); break;
+    case 6: if (!strcmp(Userlogeado->nombreUsuario, "admin")) {
+                procesarCuentasUsuarios(); system("pause"); system("cls"); break;
+            }
     default:
         system("cls");
         printf("\t\t\t\t\t\033[0;31m[X]Error opcion incorrecta[X]\033[0m\n");
+        menuJuegos(Userlogeado);
         break;
     }
 }
@@ -56,7 +57,6 @@ void imprimirTitulos() {
     printf("\t\t\t\t3. Ahorcado\n");
     printf("\t\t\t\t4- TaTeTi vs Bot\n");
     printf("\t\t\t\t5- Salir\n");
-
 }
 
 bool compararSaldo(int saldo) {
